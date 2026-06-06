4a9c33328898aa3db75a44319b86b8361957783d
Andrea Zonca <code@andreazonca.com>
2026-06-05 22:30:34 -0700

    Cleanup temporary files and fix build issues

--- push_fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TOKEN=$(gh auth token)
-git remote set-url origin "https://zonca:$TOKEN@github.com/zonca/zonca.dev.git"
-git push origin gh-pages
--- push_quest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TOKEN=$(gh auth token)
-git remote set-url origin "https://zonca:$TOKEN@github.com/zonca/quest.git"
-git push origin main
